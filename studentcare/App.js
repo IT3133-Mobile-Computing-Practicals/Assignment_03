2101cf152b6b7d664ed6f624a743d94048ad7367
TharinduLakshimal <tharindulakshimal@gmail.com>
2025-01-03 02:28:03 +0530

    style: add style for header

--- studentcare/App.js
+++ studentcare/App.js
@@ -1,8 +1,8 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginPage from './components/LoginPage';
+import * as React from "react";
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginPage from "./components/LoginPage";
 
 const Stack = createNativeStackNavigator();
 
@@ -16,10 +16,25 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="UoV Student Care">
-        <Stack.Screen name="UoV Student Care" component={LoginPage} options={{
-            headerShown: false, // Hide the header for this screen
-          }}/>
-        <Stack.Screen name="Dashboard" component={Dashboard}  />
+        <Stack.Screen
+          name="UoV Student Care"
+          component={LoginPage}
+          options={{
+            headerShown: true,
+            headerStyle: {
+              backgroundColor: "purple",
+            },
+            headerTitleStyle: {
+              color: "white",
+              fontSize: 24,
+              fontWeight: "bold",
+            },
+            headerTitleAlign: "center",
+            headerTintColor: "white",
+          }}
+        />
+
+        <Stack.Screen name="Dashboard" component={Dashboard} />
       </Stack.Navigator>
     </NavigationContainer>
   );
